c4496da8c969da9e9c412ea7e3e56941b5cc6f67
Domi <guests@mac.local>
2025-12-28 02:59:00 -0700

    worked on JS and CSS. JS completed. Working on CSS so thePrompt will look nice

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const allowFlavors = ("chocolate", "vanilla", "strawberry", "coffee")
+const allowedFlavors = ["Chocolate", "Vanilla", "Strawberry", "Coffee "]
 const userInput = prompt ("Choose your favorite flavor(s) (comma separated): Chocolate, Vanilla, Strawberry, Coffee");
 // Note: going to be adding flavorList to HTML for looks
-const flavorList = userInput.split(",")
-                            .map(item => item.trim().toLowerCase());
-const flavorCount = {
-    vanilla: 0,
-    chocolate: 0,
-    strawberry: 0,
-    coffee: 0,
-};
+const flavorList = userInput.split(",").map(item => item.trim().toLowerCase());
 
-const logOutPut = document.getElementById("flavorResults")
+const logOutPut = document.getElementById("ticketOutBox")
 if (userInput === null){
-    logOutPut.textContent = "You canceled. Nothing was logged"
+    logOutPut.textContent = "Order canceled. Nothing was logged";
+} else {
+    const flavorList = userInput
+        .split(",")
+        .map(item => item.trim().toLowerCase());
 }
 
-for (let flavor of flavorList) {
-    console.log(flavor);
+
+for (let flavor of flavorList){
+    if (allowedFlavors.includes(flavor));
+        console.log(flavor);
 
 }
 
