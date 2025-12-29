0b83b971031f708085439f643f5b7ec57c3e4993
Domi <guests@mac.local>
2025-12-29 11:12:13 -0700

    added some JS to link to my HTML since I want to use the submit button and have it be working function for my portfoliio.
    ls

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const userInput = prompt ("Choose your favorite flavor(s) (comma separated): Cho
 // Note: going to be adding flavorList to HTML for looks
 const flavorList = userInput.split(",").map(item => item.trim().toLowerCase());
 
+// NTS: the submit event belongs to form for JS, not the button itself
+const form = document.getElementById("flavorBox")
+// this is logging user input from HTML
 const logOutPut = document.getElementById("ticketOutBox")
+// NTS: This function is adding the Event(submit) to the form
+form.addEventListener("submit", function(event)){
+// NTS: This function is to stop the page from refreshing
+    event.preventDefault();
+}
 if (userInput === null){
     logOutPut.textContent = "Order canceled. Nothing was logged";
 } else {
