cfad131ce9565ac02134b2bbe3b0cae8091bfa2a
Domi <guests@mac.local>
2026-01-01 06:57:46 -0700

    saving sicne I'm not sure what changes I made.. ha

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-const allowedFlavors = ["Chocolate", "Vanilla", "Strawberry", "Coffee "]
+const allowedFlavors = ["chocolate", "vanilla", "strawberry", "coffee"]
+const flavorCount = {
+    chocolate: 0,
+    vanilla: 0,
+    strawberry: 0,
+    coffee: 0,
+}
 const userInput = prompt ("Choose your favorite flavor(s) (comma separated): Chocolate, Vanilla, Strawberry, Coffee");
 // Note: going to be adding flavorList to HTML for looks
 const flavorList = userInput.split(",").map(item => item.trim().toLowerCase());
 
-// NTS: the submit event belongs to form for JS, not the button itself
-const form = document.getElementById("flavorBox")
-// this is logging user input from HTML
-const logOutPut = document.getElementById("ticketOutBox")
-// NTS: This function is adding the Event(submit) to the form
-form.addEventListener("submit", function(event)){
-// NTS: This function is to stop the page from refreshing
+// // NTS: the submit event belongs to form for JS, not the button itself
+const logForm = document.getElementById("ticketOutBox");
+// // NTS: this is logging user input from HTML
+const logInput = document.getElementById("orderInput");
+// // NTS: this is transfer the input to output
+const logOutPut = document.getElementById("orderOutPut");
+// // NTS: This function is adding the Event(submit) to the form
+logForm.addEventListener("submit", function(event) {
     event.preventDefault();
-}
+    // // NTS: This function is to stop the page from refreshing^
+});
 if (userInput === null){
-    logOutPut.textContent = "Order canceled. Nothing was logged";
+    // textContent: grabs from HTML input
+    logOutPut.textContent = "Order canceled. Nothing was logged"; 
 } else {
     const flavorList = userInput
         .split(",")
@@ -22,8 +31,9 @@ if (userInput === null){
 
 
 for (let flavor of flavorList){
-    if (allowedFlavors.includes(flavor));
+    if (allowedFlavors.includes(flavor));{
+        flavorCount[flavor] += 1;
         console.log(flavor);
-
+    }
 }
 
