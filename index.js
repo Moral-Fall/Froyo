96385975828a8df38d39814e3b7a20dc8f81c743
Domi <guests@mac.local>
2026-01-14 13:04:09 -0700

    updating code. Submitting what I have to turn in. Will be keep updating as time goes on

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const allowedFlavors = ["chocolate", "vanilla", "strawberry", "coffee"]
-const flavorCount = {
-    chocolate: 0,
-    vanilla: 0,
-    strawberry: 0,
-    coffee: 0,
+const flavorCount(flavorList) {
+    const count = 0
 }
-// using the OR logic since this makes more sense to me. Hope that is okay :)
-flavorCount[flavors] = (flavorCount[flavors] || 0 ) + 1
+for ( let flavor of flavorList) {
+    if (allowedFlavors.includes(flavor))
+       // using the OR logic since this makes more sense to me. Hope that is okay :)
+        flavorCount[flavors] = (flavorCount[flavors] || 0 ) + 1 {
+            return counts;
+        }
+}
 
 // // NTS: the submit event belongs to form for JS, not the button itself
 const logForm = document.getElementById("ticketOutBox");
@@ -18,12 +20,14 @@ const logOutPut = document.getElementById("orderOutPut");
 logForm.addEventListener("submit", function(event) {
     event.preventDefault();
     // this is grabbing from the textarea in HTML
-const userInput = logInput.value.trim();
+    
+    // const userInput = logInput.value.trim();
+
 if (userInput === null){
     // textContent: grabs from HTML input
     logOutPut.textContent = "Order canceled. Nothing was logged"; 
 } else {
-    const flavorList = userInput
+    return flavorList = userInput
         .split(",") 
         .map(item => item.trim().toLowerCase());
 }
